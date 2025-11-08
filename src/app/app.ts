9b2f392a85dfbfbbd60e32275c46b1d2db788155
Angel Valle <rabiel2114mail.com>
2025-11-07 23:47:37 -0500

    Se creo archivo angular con tailwind y se añadio carpeta de la ventana principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// ARREGLO 1: Importamos la clase "Principal"
+import { Principal } from './principal/principal'; 
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  
+  // ARREGLO 2: Usamos "Principal" en el array de imports
+  imports: [RouterOutlet, Principal], 
+  
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/principal/principal.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-principal',
+  imports: [],
+  templateUrl: './principal.component.html',
+  standalone: true,
+  styleUrl: './principal.css',
+})
+export class Principal {
+
+}
